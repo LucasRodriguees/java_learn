978d6d65e7e8f81c8f7dfa0298c62bb19ce1e662
Lucas <lucas.orodrigues@hotmail.com>
2022-01-07 20:19:36 -0300

    primeirotestejava

---
+++ sintaxe-media/src/testevariaveis1.java
@@ -0,0 +1,15 @@
+
+public class testevariaveis1 {
+	
+	public static void main(String[] args) {
+		
+       System.out.println("ola novo teste"); 
+       
+       int idade;
+       idade = 20;
+       
+       System.out.println("idade é "+ idade + ", Parabéns!");
+    
+       
+	}
+}
---
+++ sintaxe-variaveis-e-fluxo/src/TestaEscopo.java
@@ -0,0 +1,26 @@
+
+public class TestaEscopo {
+	 public static void main(String[] args) {
+			System.out.println("Testando condicionais");
+			
+			int idade = 16;
+			int quantidadePessoas = 3;
+			boolean acompanhado;
+			
+			if (quantidadePessoas >= 2) {
+				 acompanhado = true;
+			} else {
+				acompanhado = false;
+			}
+			
+					
+			if (idade >= 18 && acompanhado) {
+			
+				System.out.println("seja bem vindo");			
+				} else {				
+				System.out.println("infelizmente voce nao pode entrar");
+		
+
+}
+}
+}
